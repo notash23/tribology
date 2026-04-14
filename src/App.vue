00d077ffcd19293ecccff2427d6d96d173abff6f
Lolorisotto <lorisvullion@gmail.com>
2026-04-14 13:38:37 -0400

    Fix html and css

--- src/App.vue
+++ src/App.vue
@@ -78,54 +78,88 @@ const setChartOptions = () => {
 }
 </script>
 
+
+
+
 <template>
-  <h1>Lubrication Theory</h1>
-  
-  <p>Welcome to more simulator of liquid in a box! Below you can find a video of the simulation done on python as a video.</p>
-  <Card class="plot">
-    <template #content>
-      <video autoplay muted loop >
-          <source src="./assets/animation.mp4" type="video/mp4">\
-          Your browser does not support the video tag.
-      </video>
-    </template>
-  </Card>
-  
-  <p>This is a simulator of the same algorithm running on this website. Play around and see what you find!</p>
-  <div class="slider-horizontal">
-    <h3>L</h3>
-    <Slider v-model="height" style="width: 75%;"/>
-    <h3>{{ height }}</h3>
-  </div>
-  
-  <div class="flex-horizontal">
-    <div class="slider-vertical">
-      <h3>U</h3>
-      <Slider class="" v-model="speed" orientation="vertical"/>
-      <h3>{{ speed }}</h3>
-    </div>
+  <div class="main">
+    <section>
+        <h1>Lubrication Theory</h1>
+        <p>Welcome to more simulator of liquid in a box! Below you can find a video of the simulation done on python as a video.</p>
+        <Card class="plot">
+            <template #content>
+                <video autoplay muted loop >
+                    <source src="./assets/animation.mp4" type="video/mp4">\
+                    Your browser does not support the video tag.
+                </video>
+            </template>
+        </Card>
+    </section>
     
-    <Card class="plot">
-      <template #content>
-        <Chart type="scatter" :data="chartData" :options="chartOptions" />
-      </template>
-    </Card>
-    
-    
-    <div class="slider-vertical">
-      <h3>P</h3>
-      <Slider v-model="pressure_grad" orientation="vertical"/>
-      <h3> {{ pressure_grad }}</h3>
-    </div>
+
+    <section>
+        <p>This is a simulator of the same algorithm running on this website. Play around and see what you find!</p>
+        <div class="slider-horizontal">
+            <h3>L</h3>
+            <Slider v-model="height" style="width: 100%;"/>
+            <h3>{{ height }}</h3>
+        </div>
+        
+        <div class="flex-horizontal">
+            <div class="slider-vertical">
+            <h3>U</h3>
+            <Slider class="" v-model="speed" orientation="vertical"/>
+            <h3>{{ speed }}</h3>
+            </div>
+            
+            <Card class="plot">
+            <template #content>
+                <Chart type="scatter" :data="chartData" :options="chartOptions" />
+            </template>
+            </Card>
+            
+            
+            <div class="slider-vertical">
+            <h3>P</h3>
+            <Slider v-model="pressure_grad" orientation="vertical"/>
+            <h3> {{ pressure_grad }}</h3>
+            </div>
+        </div>
+    </section>
+
   </div>
 </template>
 
+
+
 <style scoped>
+
+body {
+  margin: 0px;
+}
+
+.main {
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+    height: 100%;
+    overflow-y: hidden;
+    gap: 80px;
+    padding-bottom: 30px;
+}
+
+section {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+
 h1 {
   font-family: system-ui, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
   color: #91939c;
   margin-top: 20pt;
   margin-left: 20pt;
+  align-self:baseline;
 }
 
 p, h3 {
@@ -134,8 +168,13 @@ p, h3 {
 }
 
 .plot {
-  width: 75%;
-  margin: auto;
+  width: 90%;
+    max-width: calc(100vh - 80px);
+  overflow: hidden;
+}
+
+.plot * {
+    width: 100%;
 }
 
 .slider-vertical {
@@ -143,25 +182,36 @@ p, h3 {
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  
+  width: 62px;
+  max-height: calc(100% - 40px);
+
 }
-.slider-vertical > Slider {
-  width: 800px;
+.slider-vertical > div{
+  flex:1;
 }
 
 .slider-horizontal {
+  /* width: calc(100% - 100px); */
+  max-width: calc(100vh);
   width: 100%;
+  padding: 0 60px;
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: space-evenly;
   align-items: center;
-  margin-bottom: 15pt;
+  /* margin-bottom: 15pt; */
 }
 
 .flex-horizontal {
   display: flex;
-  justify-content: space-evenly;
-  align-items: center;
+  justify-content: center;
+  /* align-items: center; */
+  width: 100%;
+}
+
+.flex-horizontal .plot{
+  flex:1;
+  
 }
 </style>
